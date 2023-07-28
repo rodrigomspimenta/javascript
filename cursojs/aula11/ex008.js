cc68218d91e228259092269f251b3af570a3954e
rodrigomspimenta <137445152+rodrigomspimenta@users.noreply.github.com>
2023-07-28 17:07:18 -0300

    Ex008

--- cursojs/aula11/ex008.js
+++ cursojs/aula11/ex008.js
@@ -1,4 +1,7 @@
-var vel = 60.5;
+var vel = 61;
 
-console.log(`A velocidade do seu carro é ${vel} Km/h`);
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+if (vel > 60) { // Condição Simples
+  console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+}
 console.log(`Dirija sempre usando cinto de segurança!`);
