b0bdc04b6a7cb030daabf254e1a9bdefb01db585
rodrigomspimenta <137445152+rodrigomspimenta@users.noreply.github.com>
2023-08-07 16:21:49 -0300

    aula13

--- cursojs/aula13/ambiente.js
+++ cursojs/aula13/ambiente.js
@@ -1,8 +1,20 @@
+// Estrutura de repatição com teste lógico no final
 var c = 1;
-while (c <= 3) {
-  console.log("Tudo bem?");
-  c++; 
+do {
+  console.log(`Passo ${c}`);
+  c++;
+} while (c <= 6);
+
+// Estrutura de repatição com teste lógico no início
+/*
+
+var c = 1;
+while (c <= 10) {
+  console.log(`Passo ${c}`);
+  c++;
 }
+
+*/
 
 /*
 console.log('Tudo bem?')
