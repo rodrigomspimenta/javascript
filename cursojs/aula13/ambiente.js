9feeaa71975cf388343026223aaef1e4fd58f0af
rodrigomspimenta <137445152+rodrigomspimenta@users.noreply.github.com>
2023-08-07 12:43:59 -0300

    aula 13

---
+++ cursojs/aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1;
+while (c <= 3) {
+  console.log("Tudo bem?");
+  c++; 
+}
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
