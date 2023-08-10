09479096f2049194adb49a82f6b82e1d5261bee8
rodrigomspimenta <137445152+rodrigomspimenta@users.noreply.github.com>
2023-08-10 17:51:21 -0300

    aula 14

--- cursojs/aula14/ambiente.js
+++ cursojs/aula14/ambiente.js
@@ -5,8 +5,8 @@ while (c <= 5) {
 }
 */
 
-console.log('Vai começar')
-for(var c = 1; c <= 10; c++) {
-    console.log(c)
+console.log("Vai começar");
+for (var c = 1; c <= 10; c++) {
+  console.log(c);
 }
-console.log('F')
+console.log("Fim");
